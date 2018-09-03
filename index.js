41d820bbfcc81ce9db65473d8670c804830471d2
MariaSilvania <silvania.lopes.simoes@gmail.com>
2018-09-03 01:23:48 -0300

    add testes na biblioteca

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
-exports.hello = function () {
-    console.log("hello people !!");
+module.exports.cardValidator = function
+cardValidator(number) {
+    try {
+        var array = [];
+        var arrCalculado = [];
+
+        if ((number !== "") && (isNaN(number) == false)) {
+            var valor = 0;
+            var valorX = 0;
+            var x = 0;
+            var y = 0;
+            var soma = 0;
+
+            for (var i = 0; i < number.length; i++) {
+                array.push(number[i]);
+            }
+            array.reverse();
+
+            for (var y = 0; y < number.length; y++) {
+                if ([y] % 2 !== 0) {
+                    valor = array[y] * 2;
+                    if (valor >= 10) {
+                        x = parseInt(valor.toString().slice(0, 1));
+                        z = parseInt(valor.toString().slice(1, 2));
+                        valorX = x + z;
+                        arrCalculado.push(valorX);
+                    } else {
+                        arrCalculado.push(array[y] * 2);
+                    }
+                } else {
+                    valorX = array[y];
+                    arrCalculado.push(valorX);
+                }
+            }
+            for (var k = 0; k < arrCalculado.length; k++) {
+                soma += parseInt(arrCalculado[k]);
+            }
+            var digitoValido = soma % 10;
+            if (digitoValido === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return "Valor invalido";
+            // throw TypeError("Valor invalido");
+        }
+    } catch (e) {
+        console.log(e);
+    }
 }
---
+++ teste/valida_cartao.spec.js
@@ -0,0 +1,33 @@
+var mocha = require("mocha");
+var chai = require("chai");
+var validaCartao = require("../index");
+var expect = chai.expect;
+
+
+describe.only("validaCartao", function () {
+    describe("#cardValidator", function () {
+        it("Quando nao informar numero do cartao", function () {
+            expect(validaCartao.cardValidator(""))
+                .to.deep.equal("Valor invalido");
+        });
+    });
+    describe("#cardValidator", function () {
+        it("Quando informar numero do cartao valido", function () {
+            expect(validaCartao.cardValidator("4061683100953349"))
+                .to.deep.equal(true);
+        });
+    });
+    describe("#cardValidator", function () {
+        it("Quando informar numero do cartao invalido", function () {
+            expect(validaCartao.cardValidator("406168310095334"))
+                .to.deep.equal(false);
+        });
+    });
+    describe("#cardValidator", function () {
+        it("Quando informar uma string", function () {
+            expect(validaCartao.cardValidator("sil"))
+                .to.deep.equal("Valor invalido");
+        });
+    });
+
+});
