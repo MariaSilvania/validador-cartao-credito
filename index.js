2fe14456bf2bf2bdf9b43e54e0ae7f77a22ab64f
MariaSilvania <silvania.lopes.simoes@gmail.com>
2018-09-06 17:04:52 -0300

    a

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
-module.exports.cardValidator = function
-cardValidator(number) {
-  let array = [];
-  let arrCalculado = [];
-  let arrValores = [];
-  let indiceImpar, somaValorPar, primeiroDigito, segundoDigito, soma = 0;
-  if (number === "") {
-    throw TypeError("Digite um numero de cartão");
-  }
-  if (isNaN(number) == true) {
-    throw TypeError("Valor invalido");
-  }
-  if (number.length < 16) {
-    throw TypeError("Numero de cartao invalido");
-  }
-  for (var i = 0; i < number.length; i++) {
-    array.push(number[i]);
-  }
-  array.reverse();
+module.exports.cardValidator =
+  function cardValidator(number) {
 
-  arrValores = manipularValores();
+    let array = [];
+    let arrCalculado = [];
+    let indiceImpar, somaValorPar, primeiroDigito, segundoDigito, soma = 0;
 
-  for (var k = 0; k < arrValores.length; k++) {
-    soma += parseInt(arrValores[k]);
-  }
+    if (number === "") {
+      throw TypeError("Digite o numero do cartao");
+    }
+    if (isNaN(number) == true) {
+      throw TypeError("Valor invalido");
+    }
+    if (number.length < 16) {
+      throw TypeError("O cartao deve ter mais de 15 digito");
+    }
+    for (var i = 0; i < number.length; i++) {
+      array.push(number[i]);
+    }
+    array.reverse();
 
-  var digitoValido = soma % 10;
-  if (digitoValido === 0) {
-    return true;
-  } else {
-    return false;
-  }
-  function manipularValores() {
     array.map((numero, indice) => {
       if ([indice] % 2 !== 0) {
         indiceImpar = numero * 2;
@@ -47,6 +35,15 @@ cardValidator(number) {
         arrCalculado.push(somaValorPar);
       }
     });
-    return arrCalculado;
-  }
-}
+
+    for (var k = 0; k < arrCalculado.length; k++) {
+      soma += parseInt(arrCalculado[k]);
+    }
+    var digitoValido = soma % 10;
+
+    if (digitoValido === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
