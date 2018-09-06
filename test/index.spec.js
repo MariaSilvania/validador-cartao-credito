80f597402ba442eceb4fad42e4fc5585dd02230d
MariaSilvania <silvania.lopes.simoes@gmail.com>
2018-09-06 19:03:57 -0300

    d

--- test/index.spec.js
+++ test/index.spec.js
@@ -7,23 +7,23 @@ var expect = chai.expect;
 describe.only("validaCartao", function () {
     describe("#cardValidator", function () {
         it("Quando nao houver parametro", function () {
-            expect(() => validaCartao.cardValidator("")).to.throw();
+            expect(() => validaCartao("")).to.throw();
         });
         it("Quando o numero for uma string", function () {
-            expect(() => validaCartao.cardValidator("sil")).to.throw();
+            expect(() => validaCartao("sil")).to.throw();
         });
         it("Quando o numero for um inteiro e houver um digito", function () {
-            expect(() => validaCartao.cardValidator("1")).to.throw();
+            expect(() => validaCartao("1")).to.throw();
         });
         it("Quando o numero for um inteiro e houver menos de 16 digitos", function () {
-            expect(() => validaCartao.cardValidator("45394015278")).to.throw();
+            expect(() => validaCartao("45394015278")).to.throw();
         });
         it("Quando informar numero do cartao valido", function () {
-            expect(validaCartao.cardValidator("4798846349843569"))
+            expect(validaCartao("4798846349843569"))
                 .to.deep.equal(true);
         });
         it("Quando informar numero do cartao invalido", function () {
-            expect(validaCartao.cardValidator("4539401527887372"))
+            expect(validaCartao("4539401527887372"))
                 .to.deep.equal(false);
         });
     });
